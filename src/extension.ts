28028683b6c110c8345d325ff39dba2adceba0d0
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-11-03 14:14:17 -0500

    Refactor text to image conversion functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,109 +1,53 @@
 import * as vscode from "vscode";
-const path = require("path");
-const fs = require("fs");
+import * as path from "path";
+import { readHtml } from "./utils";
 
-export function activate(context: vscode.ExtensionContext) {
-
-  let disposable = vscode.commands.registerCommand(
-    "capture-code.captureText",
-    () => {
-      let editor = vscode.window.activeTextEditor;
-      if (editor) {
-        let selection = editor.selection;
-        let text = editor.document.getText(selection);
-
-		const panel = vscode.window.createWebviewPanel(
-          "textToImage",
-          "Text to Image", 
-          vscode.ViewColumn.Two,
-          {
-            enableScripts: true,
-          }
-        );
-
-		vscode.window.onDidChangeTextEditorSelection((event) => {
-			if (event.textEditor === editor) { 
-			  let newText = event.textEditor.document.getText(event.selections[0]);
-			  panel.webview.postMessage({ command: "createImage", text: newText });
-			}
-		  });
-		  
-
-        const scriptPathOnDisk = vscode.Uri.file(
-          path.join(context.extensionPath, "src", "script.js")
-        );
-        const scriptUri = panel.webview.asWebviewUri(scriptPathOnDisk);
-
-        const domtoImageUrl = vscode.Uri.file(
-          path.join(
-            context.extensionPath,
-            "node_modules",
-            "dom-to-image-even-more",
-            "dist",
-            "dom-to-image-more.min.js"
-          )
-        );
-
-        const domtoImageUri = panel.webview.asWebviewUri(domtoImageUrl);
-
-        const scriptPathcPrismjs = vscode.Uri.file(
-          path.join(
-            context.extensionPath,
-            "node_modules",
-            "prismjs",
-            "prism.js"
-          )
-        );
-
-        const scriptUriPrismjs = panel.webview.asWebviewUri(scriptPathcPrismjs);
-
-        const stylePathcPrismjs = vscode.Uri.file(
-          path.join(
-            context.extensionPath,
-            "node_modules",
-            "prismjs",
-            "themes",
-            "prism-tomorrow.css"
-          )
-        );
-
-        const styleUriPrismjs = panel.webview.asWebviewUri(stylePathcPrismjs);
-
-        const myStylePath = vscode.Uri.file(
-          path.join(context.extensionPath, "src", "style.css")
-        );
-
-        const myStyleUri = panel.webview.asWebviewUri(myStylePath);
-
-
-        const filePath = path.join(context.extensionPath, "src", "index.html");
-
-        const htmlFile = fs.readFileSync(filePath, "utf-8");
-
-        let updatedHtml = htmlFile;
-        updatedHtml = updatedHtml.replace(
-          "${styleUriPrismjs}",
-          styleUriPrismjs
-        );
-        updatedHtml = updatedHtml.replace("${myStyleUri}", myStyleUri);
-        updatedHtml = updatedHtml.replace("${domtoImageUri}", domtoImageUri);
-        updatedHtml = updatedHtml.replace(
-          "${scriptUriPrismjs}",
-          scriptUriPrismjs
-        );
-
-        updatedHtml = updatedHtml.replace("${scriptUri}", scriptUri);
-
-        panel.webview.html = updatedHtml;
-
-        panel.webview.postMessage({ command: "createImage", text: text });
-
-
-      }
+const createPanel = async (context: vscode.ExtensionContext) => {
+  const panel = vscode.window.createWebviewPanel(
+    "textToImage",
+    "Capture Code",
+    { viewColumn: vscode.ViewColumn.Beside, preserveFocus: true },
+    {
+      enableScripts: true,
+      localResourceRoots: [vscode.Uri.file(context.extensionPath)],
     }
   );
 
+  panel.webview.html = await readHtml(
+    path.resolve(context.extensionPath, "webview/index.html"),
+    panel
+  );
+
+  return panel;
+};
+
+const runCommand = async (context: vscode.ExtensionContext) => {
+  let editor = vscode.window.activeTextEditor;
+  if(!editor) return null
+  let selection = editor.selection;
+  let languageId = editor.document.languageId;
+  let text = editor.document.getText(selection);
+  
+  const panel = await createPanel(context);
+
+  panel.webview.postMessage({ command: "createImage", text: text, languageId });
+
+
+  vscode.window.onDidChangeTextEditorSelection((event) => {
+    if (event.textEditor === editor) {
+      let newText = event.textEditor.document.getText(event.selections[0]);
+      panel.webview.postMessage({ command: "createImage", text: newText, languageId });
+    }
+  });
+};
+
+export const activate = (context: vscode.ExtensionContext) => {
+  let disposable = vscode.commands.registerCommand(
+    "capture-code.captureText",
+    () => runCommand(context)
+  );
+
   context.subscriptions.push(disposable);
-}
+};
 
 export function deactivate() {}
