94480e0be69ea1a935c8b6e8523bfa2eaf7acfbf
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2024-01-06 13:14:31 -0500

    formattig code

--- src/extension.ts
+++ src/extension.ts
@@ -23,29 +23,30 @@ const createPanel = async (context: vscode.ExtensionContext) => {
 
 const runCommand = async (context: vscode.ExtensionContext) => {
   let editor = vscode.window.activeTextEditor;
-  if(!editor) return null
+  if (!editor) return null;
   let selection = editor.selection;
   let languageId = editor.document.languageId;
   let text = editor.document.getText(selection);
-  
   const panel = await createPanel(context);
 
   panel.webview.postMessage({ command: "createImage", text: text, languageId });
 
-
   vscode.window.onDidChangeTextEditorSelection((event) => {
     if (event.textEditor === editor) {
       let newText = event.textEditor.document.getText(event.selections[0]);
-      panel.webview.postMessage({ command: "createImage", text: newText, languageId });
+      panel.webview.postMessage({
+        command: "createImage",
+        text: newText,
+        languageId,
+      });
     }
   });
 
-  panel.webview.onDidReceiveMessage((message) => {
+  panel.webview.onDidReceiveMessage(async (message) => {
     if (message.command === "openExternalUrl") {
       vscode.env.openExternal(vscode.Uri.parse(message.url));
     }
   });
-  
 };
 
 export const activate = (context: vscode.ExtensionContext) => {
